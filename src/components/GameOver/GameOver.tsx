efe6d7f736944b98b163c7d0e9e38a559b67b835
Ben Hodgson <ben.hodgson@lendable.co.uk>
2024-11-24 17:18:51 +0000

    Remove leftover bottom margin from button

--- src/components/GameOver/GameOver.tsx
+++ src/components/GameOver/GameOver.tsx
@@ -17,7 +17,7 @@ const GameOver = () => {
         {t("roundScore", { score: currentScore, rounds: currentRound })}
       </p>
       <button
-        className="w-full max-w-48 mb-12 bg-orange-500 dark:bg-orange-700 hover:bg-orange-300 dark:hover:bg-orange-400 text-white text-xl font-bold py-4 px-5 rounded-full transition duration-200 transform hover:scale-105"
+        className="w-full max-w-48 bg-orange-500 dark:bg-orange-700 hover:bg-orange-300 dark:hover:bg-orange-400 text-white text-xl font-bold py-4 px-5 rounded-full transition duration-200 transform hover:scale-105"
         onClick={handleReplay}
       >
         {t("playAgain")}
