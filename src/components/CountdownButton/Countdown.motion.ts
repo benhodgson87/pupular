5bc0448b6d4ed673ed8fbc8a34320875c6c4d6d0
Ben Hodgson <ben.hodgson@lendable.co.uk>
2024-11-24 17:56:20 +0000

    Handle dark mode colours in countdown timer

--- src/components/CountdownButton/Countdown.motion.ts
+++ src/components/CountdownButton/Countdown.motion.ts
@@ -1,16 +1,29 @@
-export const countdownButtonAnimation = (countdown: number) => ({
-  initial: {
-    backgroundImage:
-      "linear-gradient(90deg, var(--tw-orange-500) 100%, var(--tw-orange-400) 100%)",
-  },
-  animate: {
-    backgroundImage:
-      "linear-gradient(90deg, var(--tw-orange-500) 0%, var(--tw-orange-400) 0%)",
-    transition: {
-      type: "tween",
-      ease: "linear",
-      duration: countdown,
-      repeat: 0,
+export const countdownButtonAnimation = (countdown: number) => {
+  const isDarkMode =
+    window &&
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const baseColor = isDarkMode
+    ? "var(--tw-orange-700)"
+    : "var(--tw-orange-500)";
+
+  const timerColor = isDarkMode
+    ? "var(--tw-orange-600)"
+    : "var(--tw-orange-400)";
+
+  return {
+    initial: {
+      backgroundImage: `linear-gradient(90deg, ${timerColor} 100%, ${baseColor} 100%)`,
     },
-  },
-});
+    animate: {
+      backgroundImage: `linear-gradient(90deg, ${timerColor} 0%, ${baseColor} 0%)`,
+      transition: {
+        type: "tween",
+        ease: "linear",
+        duration: countdown,
+        repeat: 0,
+      },
+    },
+  };
+};
