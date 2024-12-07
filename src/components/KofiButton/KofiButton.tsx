6e24ac671c62c304391a67411328bee8ef630585
Ben Hodgson <ben.hodgson@lendable.co.uk>
2024-12-07 18:30:53 +0000

    Update Biome config, make Kofi button size configurable

--- src/components/KofiButton/KofiButton.tsx
+++ src/components/KofiButton/KofiButton.tsx
@@ -1,13 +1,22 @@
+import classNames from "classnames";
 import { useTranslation } from "react-i18next";
 import { LINK_EXT_BH_KOFI } from "~/config/links";
 
-const KofiButton = () => {
+type Props = {
+  size?: "sm" | "md" | "lg";
+};
+
+const KofiButton = ({ size = "sm" }: Props) => {
   const { t } = useTranslation(undefined, { keyPrefix: "KofiButton" });
 
   return (
     <a href={LINK_EXT_BH_KOFI} target="_blank">
       <img
-        className="border-0 h-7"
+        className={classNames("border-0", {
+          "h-7": size === "sm",
+          "h-12": size === "md",
+          "h-16": size === "lg",
+        })}
         src="https://storage.ko-fi.com/cdn/kofi1.png?v=6"
         alt={t("altText")}
       />
